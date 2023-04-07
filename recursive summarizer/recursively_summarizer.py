db08fd842cde2002fa8c232d87e8304edd5e28bf
Vichentito <chente54321@gmail.com>
2023-04-06 22:55:19 -0600

    cambios

--- recursive summarizer/recursively_summarizer.py
+++ recursive summarizer/recursively_summarizer.py
@@ -11,40 +11,15 @@ def open_file(filepath):
 
 
 openai.api_key = open_file('openaikey.txt')
+history = [
+    {"role": "system",
+     "content": "Eres una IA de modelo de lenguaje entrenado para resumir textos en español."},
+]
 
 
 def save_file(content, filepath):
     with open(filepath, 'w', encoding='utf-8') as outfile:
         outfile.write(content)
-
-
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
-    max_retry = 5
-    retry = 0
-    while True:
-        try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
-                temperature=temp,
-                max_tokens=tokens,
-                top_p=top_p,
-                frequency_penalty=freq_pen,
-                presence_penalty=pres_pen,
-                stop=stop)
-            text = response['choices'][0]['text'].strip()
-            text = re.sub('\s+', ' ', text)
-            return text
-        except Exception as oops:
-            filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt +
-                              '\n\n==========\n\nRESPONSE:\n\n' + oops)
-            retry += 1
-            if retry >= max_retry:
-                return "GPT3 error: %s" % oops
-            print('Error communicating with OpenAI:', oops)
-            sleep(1)
 
 
 def selection():
@@ -72,20 +47,54 @@ def selection():
     return [os.path.join(directorio, archivos[i-1]) for i in seleccion]
 
 
+def summary(history, model='gpt-3.5-turbo', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
+    max_retry = 5
+    retry = 0
+    while True:
+        try:
+            #print('Prompt:', history)
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=history,
+                temperature=temp,
+                max_tokens=tokens,
+                top_p=top_p,
+                frequency_penalty=freq_pen,
+                presence_penalty=pres_pen,
+                stop=stop,
+            )
+            text = response['choices'][0]['message']['content'].strip()
+            text = re.sub('\s+', ' ', text)
+            return text
+        except Exception as oops:
+            retry += 1
+            if retry >= max_retry:
+                return "GPT3 error: %s" % oops
+            print('Error communicating with OpenAI:', oops)
+            sleep(1)
+
+
 if __name__ == '__main__':
     files = selection()
     # print(files)
     for file in files:
         print('\n\nProcessing', file)
         alltext = open_file(file)
-        chunks = textwrap.wrap(alltext, 4000)
+        chunks = textwrap.wrap(alltext, 1000)
         result = list()
         count = 0
         for chunk in chunks:
+            chunk = chunk.encode(encoding='ASCII', errors='ignore').decode()
+            if count == 0:
+                history.append(
+                    {"role": "user", "content": f"Resume el texto siguiente: {chunk}"},
+                )
+            else:
+                history.append(
+                    {"role": "user", "content": f"Continua el resumen con la siguiente informacion: {chunk}"},
+                )
+            summary_text = summary(history)
             count = count + 1
-            prompt = open_file('prompt.txt').replace('<<SUMMARY>>', chunk)
-            prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
-            summary = gpt3_completion(prompt)
             print('\n', file, count, 'of', len(chunks))
-            result.append(summary)
+            result.append(summary_text)
         save_file('\n\n'.join(result), 'summaries/output_%s.txt' % file[6:])
